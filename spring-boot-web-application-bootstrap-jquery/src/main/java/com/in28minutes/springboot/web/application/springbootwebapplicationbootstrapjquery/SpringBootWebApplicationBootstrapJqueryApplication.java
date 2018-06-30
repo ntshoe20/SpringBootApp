0a05f4f2d346c49cdc0a856b78a942cdae7ae71c
Itumeleng.Ntshoe <Itumeleng.Ntshoe@CPT-AA012454.CSGICORP.COM>
2018-06-30 16:02:58 +0200

    Springboot app example from Dzone

---
+++ spring-boot-web-application-bootstrap-jquery/src/main/java/com/in28minutes/springboot/web/application/springbootwebapplicationbootstrapjquery/SpringBootWebApplicationBootstrapJqueryApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.springboot.web.application.springbootwebapplicationbootstrapjquery;
+
+/*
+ * https://dzone.com/articles/spring-boot-integrating-with-bootstrap-and-jquery
+ * 2018-05-31
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplicationBootstrapJqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
+	}
+}
